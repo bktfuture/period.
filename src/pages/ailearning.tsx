c47b22546a9c6612263cfae2e1b9e0b413803b56
Aiturgan <aiturgan888@gmail.com>
2024-03-09 20:13:31 -0600

    rebuild

---
+++ src/pages/ailearning.tsx
@@ -0,0 +1,11 @@
+export default function AiLearning() {
+	return (
+		<>
+			<p>
+				Women make up more than half the U.S. population and about half the workforce. Moreover, women are more likely than men to be caregivers, and
+				make 80 percent of all health care decisions. Yet the medical sciences continue to underfund studies focused on women, even among diseases that
+				affect women most of all. This is, quite simply, inefficient science.
+			</p>
+		</>
+	);
+}
